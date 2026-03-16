847052b0bedcc1c80feaacc09f12ecce0c5d47c9
KJH <godgodgod96@naver.com>
2026-03-16 14:57:42 +0900

    16 MAR 26

---
+++ Ex0103_BubbleSort/Ex0103_BubbleSort.cpp
@@ -0,0 +1,56 @@
+﻿#include <iostream>
+
+using namespace std;
+
+bool CheckSorted(int* arr, int size)
+{
+	for (int i = 0; i < size - 1; i++)
+	{
+		if (arr[i] > arr[i + 1])
+			return false;
+	}
+
+	return true;
+}
+
+void Print(int* arr, int size)
+{
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
+
+int main()
+{
+	//int arr[] = { 5, 1, 4, 2, 8 }; // 위키피디아 예시
+	//int arr[] = { 5, 4, 3, 2, 1 }; // Worst Case
+	int arr[] = { 1, 2, 3, 4, 5 }; // Best Case
+	int n = sizeof(arr) / sizeof(arr[0]);
+
+	Print(arr, n);
+	cout << endl;
+
+	// Bubble Sort
+	// Best Case 조기 종료
+	bool best_case =true;
+
+	{
+		for(int i=0; i < n-1; i++){
+			//for(int j = 0; j<n -i-1; j++ ){
+			for (int j =0; j < n -1;j++){
+				if(arr[j]>arr[j+1]){
+					swap(arr[j],arr[j+1]);
+					best_case = false;
+				}
+				Print(arr,n);
+			if (best_case) break;
+			}
+			
+			cout << endl;
+		}
+	}
+	return 0;
+
+	// Best case
+	// Stability
+}
